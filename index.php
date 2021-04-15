b6ee4cfcea49e1371f24dbbb0cc7e28c59eef56a
Юрий <drunkexorzist135@gmail.com>
2021-04-15 19:21:18 +1000

    добавил js фалы

--- index.php
+++ index.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap" rel="stylesheet">
+    <script type="text/javascript" src="script/jquery.js"></script>
+    <script type="text/javascript" src="script/forms.js"></script>
 </head>
 
 <body>
