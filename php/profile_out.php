164baa278bd5870a356f82cc25103d266de959e9
Юрий <drunkexorzist135@gmail.com>
2021-06-26 20:37:26 +1000

    Update profile_out.php

--- php/profile_out.php
+++ php/profile_out.php
@@ -1,5 +1,4 @@
 <?php
-
     session_start();
     require_once "connect.php";
 
@@ -13,7 +12,6 @@
     $user_id_db = mysqli_query($connect, "SELECT * FROM `registr` WHERE `id` = 'user_id'");
     $del_time_db = mysqli_query($connect, "SELECT * FROM `order` WHERE `del_time` = 'del_time_id'");
 
-    
     if ($city_name != null & $delivery_type != null ) {
         
         mysqli_query($connect, "INSERT INTO `gruz_info` (`user_id`,`del_type`,`del_prise`,`del_city`,`del_time`) VALUES ('$user_id','$delivery_type_db','$delivery_prise_db','$city_name_db','$del_time_db')");
